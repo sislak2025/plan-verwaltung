6e54b28a68b2eb795a57246d955c7171b880b2df
sislak2025 <j.feiler@sislakdesign.de>
2026-01-09 09:57:42 +0100

    empty keys

--- classes/class-pv-mcp.php
+++ classes/class-pv-mcp.php
@@ -406,7 +406,9 @@ if (!class_exists('PV_MCP')) {
                 $additional_fields = get_field('pv_group_additional', $post_id);
                 if (is_array($additional_fields)) {
                     foreach ($additional_fields as $additional_key => $additional_field) {
-                        $acf_fields[$additional_key] = get_field($additional_key, $post_id);
+                        if (!empty($additional_key)) {
+                            $acf_fields[$additional_key] = get_field($additional_key, $post_id);
+                        }
                     }
                 }
             }
